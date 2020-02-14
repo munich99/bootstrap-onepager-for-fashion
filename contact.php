2bd803c80a1f09c396b2ed63fe5c20a474083bd9
munich99 <ask@filizanka.net>
2020-02-14 14:57:25 +0100

    alles fertig

--- contact.php
+++ contact.php
@@ -41,7 +41,69 @@ if(!empty($_POST['g-recaptcha-response']))
                 // Here goes your code.
                     $okMessage = 'Vielen Dank für Ihre Nachricht,:) wir werden uns in kürze bei Ihnen Melden!';
                     // echo 'Congratulations! You have passed the reCaptcha!';
+
+
+                                                    try
+                                                    {
+
+                                                        if(count($_POST) == 0) throw new \Exception('Form is empty');
+                                                                
+                                                        $emailText = "You have a new message from your contact form\n=============================\n";
+
+                                                        foreach ($_POST as $key => $value) {
+                                                            // If the field exists in the $fields array, include it in the email 
+                                                            if (isset($fields[$key])) {
+                                                                $emailText .= "$fields[$key]: $value\n";
+                                                            }
+                                                        }
+
+                                                        // All the neccessary headers for the email.
+                                                        $headers = array('Content-Type: text/plain; charset="UTF-8";',
+                                                            'From: ' . $from,
+                                                            'Reply-To: ' . $from,
+                                                            'Return-Path: ' . $from,
+                                                        );
+                                                        
+                                                        // Send email
+                                                        mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+                                                        $responseArray = array('type' => 'success', 'message' => $okMessage);
+                                                    }
+                                                    catch (\Exception $e)
+                                                    {
+                                                        $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+                                                    }
+
+                                                    // ------------------------------------------
+
+
+
+
+
+                                                    // if requested by AJAX request return JSON response
+                                                    if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+                                                        $encoded = json_encode($responseArray);
+
+                                                        header('Content-Type: application/json');
+
+                                                        echo $encoded;
+                                                    }
+                                                    // else just display the message
+                                                    else {
+                                                        echo $responseArray['message'];
+                                                    }
+
+
+
+
+
+
+
+
+                    
                 }
+
+                //// hier fängt das else an
                 else
                 {
                     $okMessage = 'Nachricht,:)ging schief';
@@ -58,52 +120,3 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 
 //-------------------------------
-try
-{
-
-    if(count($_POST) == 0) throw new \Exception('Form is empty');
-            
-    $emailText = "You have a new message from your contact form\n=============================\n";
-
-    foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email 
-        if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value\n";
-        }
-    }
-
-    // All the neccessary headers for the email.
-    $headers = array('Content-Type: text/plain; charset="UTF-8";',
-        'From: ' . $from,
-        'Reply-To: ' . $from,
-        'Return-Path: ' . $from,
-    );
-    
-    // Send email
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-}
-catch (\Exception $e)
-{
-    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-}
-
-// ------------------------------------------
-
-
-
-
-
-// if requested by AJAX request return JSON response
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
-
-    header('Content-Type: application/json');
-
-    echo $encoded;
-}
-// else just display the message
-else {
-    echo $responseArray['message'];
-}
